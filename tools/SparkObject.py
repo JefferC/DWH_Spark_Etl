7ea3922a3a2cebdb5795215a2932acce9c2c74c7
Lillllllllllll <38778935+JefferC@users.noreply.github.com>
2018-07-10 15:39:10 +0800

    jeffer
    1、替换了原先处理sql的方法
    2、添加注释

--- tools/SparkObject.py
+++ tools/SparkObject.py
@@ -15,27 +15,35 @@ class SparkObject:
         self.setSparkSession()
 
     def setSparkSession(self,ss=None,ifinit=True):
+        # 如果不需要初始化，则无需创建SparkSession实例
         if not ifinit:
             if isinstance(ss,SparkSession):
                 self.SpkSess = ss
                 return True
             else:
                 return False
+        # 创建SparkConf实例
         conf = SparkConf().setAppName(Config.SPARK_APPNAME).setMaster(Config.HADOOP_MASTER)
+        # 添加所有配置信息，可在Config.py中配置
         for i in Config.SPARKCONFIG:
             conf.set(i,Config.SPARKCONFIG[i])
+        # 创建SparkSession实例
         self.SpkSess = SparkSession.builder.config(conf=conf).enableHiveSupport().getOrCreate()
 
     def getSparkSession(self):
         return self.SpkSess
 
     def execSql(self,sql):
+        # 判断SparkSession是否存在，不存在则创建
         if self.SpkSess == None:
             self.setSparkSession()
+        # 状态代码： 0 成功 否则失败
         StatsCode = 0
         try:
+            # 调用Sql工具箱处理分割SQL
             for i in SqlUtil.SqlUtil.ProcessSql(sql,vars()):
                 r = self.SpkSess.sql(i)
+                # 查询操作需要打印到控制台
                 if i.upper().strip().startswith("SELECT") or i.upper().strip().startswith("SHOW"):
                     r.show(truncate=False)
         except Exception as e:
--- tools/SqlUtil.py
+++ tools/SqlUtil.py
@@ -5,26 +5,37 @@
 
 import re
 
+# 强行类一下，其实没必要
 class SqlUtil:
 
     @staticmethod
     def ProcessSql(sql,var):
-        sql = sql.strip().upper()
+        # 以下的代码会出事情。注释并引以为戒
+        # sql = sql.strip().upper()
         sql = SqlUtil.ReplaceVars(sql,SqlUtil.ProcessVars(var))
+        if not sql:
+            exit(0)
         sql = SqlUtil.SplitSql(sql)
         return sql
 
     @classmethod
     def ReplaceVars(cls,sql,var):
+        # 处理SQL，将SQL中的$var$变量替换成值
+        if not var:
+            print "Error Class: SqlUtil.SqlUtil.ReplaceVars"
+            return False
+        # 把sql中的变量都替换成值
         for i in var:
             sql = sql.replace("$" + i + "$", var[i])
         return sql
 
     @classmethod
     def ProcessVars(cls,var):
+        # 有毒就返回False
         if not isinstance(var,dict):
             return False
         r = {}
+        # 只处理value是string的变量
         for i in var:
             if isinstance(var[i],str):
                 r[i] = var[i]
@@ -32,6 +43,29 @@ class SqlUtil:
 
     @classmethod
     def SplitSql(cls,sql):
+        # 匹配引号内内容
+        reg = re.compile(r"\'(.*?)\'")
+        # 引号里内容中的分号替换成不可见字符\1
+        sql = reg.sub(SqlUtil.RegSubMethod,sql)
+        # 处理后的Sql拆分
+        sql = sql.split(";")
+        Result = []
+        # 循环sql列表将引号替换回去
+        for i in sql:
+            Result.append(i.replace('\1',';').strip())
+        return Result
+
+    @classmethod
+    def RegSubMethod(cls,match):
+        # 替换为不可见字符。
+        return match.group(0).replace(";","\1")
+
+    @classmethod
+    def SplitSql_Old(cls,sql):
+        # 这个方法比较糟糕，废弃了
+        # 基本思路是先把sql中引号内的部分拿出来。然后对Sql进行Split（担心引号里会有分号影响split）
+        # 随后再按顺序将引号里的内容再塞回去
+        # 当时就觉得特别糟糕。翌日想了个简单又好用的方式
         reg = re.compile(r"\'(.*?)\'")
         cont = reg.findall(sql)
         Done = False
