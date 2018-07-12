e447784fc63741531d507dbaed2a0149353cd586
Lillllllllllll <38778935+JefferC@users.noreply.github.com>
2018-07-12 10:58:07 +0800

    jeffer
    新建分支。
    1、SqlUtil改为非类形式（无意义修改）
    2、LogUtil增加清理日志
    3、其他相应修改
    4、增加写入日志

--- tools/LogUtil.py
+++ tools/LogUtil.py
@@ -22,6 +22,8 @@ class LogUtil:
         self.log.addHandler(file_handler)
         self.log.addHandler(console_handler)
         self.log.setLevel(logging.INFO)
+        # 清理日志 ToDo：这里不是特别好。每次实例化日志工具箱就会清理日志。考虑定时处理（例如利用定时器）
+        self.ClearLog()
 
     def wtLog(self,lv,msg):
         if self.log == None:
@@ -34,5 +36,21 @@ class LogUtil:
         elif lv == "warn":
             self.log.warn(msg)
         else:
-            self.log.critical(msg)
+            self.log.error(msg)
         return True
+
+    def ClearLog(self):
+        self.wtLog("INFO","Start Clear Logs")
+        # 保留一定时间的日志
+        moth = lambda x:datetime.datetime.strftime(datetime.datetime.today() - datetime.timedelta(days=x),'%Y-%m-%d')
+        days = xrange(Config.LOG_KEEP_DAYS)
+        keeps = map(moth,days)
+        # 获取日志目录下所有文件或路径
+        d = os.listdir(Config.LOGGING_DIR)
+        for i in d:
+            # 只关注log文件 ToDo: 这里也可以优化为准确定位到YYYY-MM-DD.log文件 后期考虑
+            if os.path.isfile(Config.LOGGING_DIR + os.sep + i) and i[-3:] == "log":
+                # 非保留项目则删除
+                if i.split(".")[0] not in keeps:
+                    os.remove(Config.LOGGING_DIR + os.sep + i)
+                    self.wtLog("INFO","Remove Log: %s" %(Config.LOGGING_DIR + os.sep + i))
--- tools/SqlUtil.py
+++ tools/SqlUtil.py
@@ -5,104 +5,105 @@
 
 import re
 
-# 强行类一下，其实没必要
-class SqlUtil:
 
-    @staticmethod
-    def ProcessSql(sql,var):
-        # 以下的代码会出事情。注释并引以为戒
-        # sql = sql.strip().upper()
-        sql = SqlUtil.ReplaceVars(sql,SqlUtil.ProcessVars(var))
-        if not sql:
-            exit(0)
-        sql = SqlUtil.SplitSql(sql)
-        return sql
 
-    @classmethod
-    def ReplaceVars(cls,sql,var):
-        # 处理SQL，将SQL中的$var$变量替换成值
-        if not var:
-            print "Error Class: SqlUtil.SqlUtil.ReplaceVars"
-            return False
-        # 把sql中的变量都替换成值
-        for i in var:
-            sql = sql.replace("$" + i + "$", var[i])
-        return sql
 
-    @classmethod
-    def ProcessVars(cls,var):
-        # 有毒就返回False
-        if not isinstance(var,dict):
-            return False
-        r = {}
-        # 只处理value是string的变量
-        for i in var:
-            if isinstance(var[i],str):
-                r[i] = var[i]
-        return r
+def ProcessSql(sql,var):
+    logger = var['self'].logger
+    # 以下的代码会出事情。注释并引以为戒
+    # sql = sql.strip().upper()
+    sql = ReplaceVars(sql,ProcessVars(var))
+    if not sql:
+        logger.wtLog("Error","Not A Sql")
+        exit(0)
+    sql = SplitSql(sql)
+    return sql
 
-    @classmethod
-    def SplitSql(cls,sql):
-        # 匹配引号内内容
-        reg = re.compile(r"\'(.*?)\'")
-        # 引号里内容中的分号替换成不可见字符\1
-        sql = reg.sub(SqlUtil.RegSubMethod,sql)
-        # 处理后的Sql拆分
-        sql = sql.split(";")
-        Result = []
-        # 循环sql列表将引号替换回去
-        for i in sql:
-            # 干掉注释内的内容。
-            res = SqlUtil.RemoveComment(i.replace('\1',';').strip())
-            # 跳过空Sql
-            if res.strip() == "":
-                continue
-            Result.append(res)
-        return Result
 
-    @classmethod
-    def RegSubMethod(cls,match):
-        # 替换为不可见字符。
-        return match.group(0).replace(";","\1")
+def ReplaceVars(sql,var):
+    # 处理SQL，将SQL中的$var$变量替换成值
+    if not var:
+        print "Error Script: SqlUtil.ReplaceVars"
+        exit(12)
+    # 把sql中的变量都替换成值
+    for i in var:
+        sql = sql.replace("$" + i + "$", var[i])
+    return sql
 
-    @classmethod
-    def RemoveComment(cls,sql):
-        reg1 = re.compile(r"\/\*[\s\S]*?\*\/")
-        reg2 = re.compile(r"--(.*)?")
-        sql = reg1.sub("",sql)
-        sql = reg2.sub("",sql)
-        return sql
 
-    @classmethod
-    def SplitSql_Old(cls,sql):
-        # 这个方法比较糟糕，废弃了
-        # 基本思路是先把sql中引号内的部分拿出来。然后对Sql进行Split（担心引号里会有分号影响split）
-        # 随后再按顺序将引号里的内容再塞回去
-        # 当时就觉得特别糟糕。翌日想了个简单又好用的方式
-        reg = re.compile(r"\'(.*?)\'")
-        cont = reg.findall(sql)
-        Done = False
-        st = 0
-        mach = 0
-        while not Done:
-            rg = reg.search(sql, st)
-            if not rg:
-                Done = True
-            else:
-                st = rg.end()
-                ss = rg.start()
-                st_str = sql[0:ss]
-                ed_str = sql[st:]
-                k = "$-%s-$" % str(mach)
-                sql = k.join([st_str, ed_str])
-                mach = mach + 1
-        sql = sql.split(";")
-        sql2 = []
-        for i in sql:
-            if i.strip() == "":
-                continue
-            for j in xrange(len(cont)):
-                k = "$-%s-$" % str(j)
-                i = i.replace(k, "'" + cont[j] + "'")
-            sql2.append(i)
-        return sql2
+def ProcessVars(var):
+    # 有毒就返回False
+    if not isinstance(var,dict):
+        return False
+    r = {}
+    # 只处理value是string的变量
+    for i in var:
+        if isinstance(var[i],str):
+            r[i] = var[i]
+    return r
+
+
+def SplitSql(sql):
+    # 匹配引号内内容
+    reg = re.compile(r"\'(.*?)\'")
+    # 引号里内容中的分号替换成不可见字符\1
+    sql = reg.sub(RegSubMethod,sql)
+    # 处理后的Sql拆分
+    sql = sql.split(";")
+    Result = []
+    # 循环sql列表将引号替换回去
+    for i in sql:
+        # 干掉注释内的内容。
+        res = RemoveComment(i.replace('\1',';').strip())
+        # 跳过空Sql
+        if res.strip() == "":
+            continue
+        Result.append(res)
+    return Result
+
+
+def RegSubMethod(match):
+    # 替换为不可见字符。
+    return match.group(0).replace(";","\1")
+
+
+def RemoveComment(sql):
+    reg1 = re.compile(r"\/\*[\s\S]*?\*\/")
+    reg2 = re.compile(r"--(.*)?")
+    sql = reg1.sub("",sql)
+    sql = reg2.sub("",sql)
+    return sql
+
+
+def SplitSql_Old(sql):
+    # 这个方法比较糟糕，废弃了
+    # 基本思路是先把sql中引号内的部分拿出来。然后对Sql进行Split（担心引号里会有分号影响split）
+    # 随后再按顺序将引号里的内容再塞回去
+    # 当时就觉得特别糟糕。翌日想了个简单又好用的方式
+    reg = re.compile(r"\'(.*?)\'")
+    cont = reg.findall(sql)
+    Done = False
+    st = 0
+    mach = 0
+    while not Done:
+        rg = reg.search(sql, st)
+        if not rg:
+            Done = True
+        else:
+            st = rg.end()
+            ss = rg.start()
+            st_str = sql[0:ss]
+            ed_str = sql[st:]
+            k = "$-%s-$" % str(mach)
+            sql = k.join([st_str, ed_str])
+            mach = mach + 1
+    sql = sql.split(";")
+    sql2 = []
+    for i in sql:
+        if i.strip() == "":
+            continue
+        for j in xrange(len(cont)):
+            k = "$-%s-$" % str(j)
+            i = i.replace(k, "'" + cont[j] + "'")
+        sql2.append(i)
+    return sql2
